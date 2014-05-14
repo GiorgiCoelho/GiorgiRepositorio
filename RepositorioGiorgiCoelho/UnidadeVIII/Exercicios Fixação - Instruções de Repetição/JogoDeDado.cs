96d03815e22b004b9c3ae3fe44cafd9bcd38e1fb
Giorgi Coelho <giorgi_coelho@hotmail.com>
2014-05-14 10:52:22 -0300

    Adição do exercício 13(jogoDeDado2) - Unid. VIII.

--- RepositorioGiorgiCoelho/UnidadeVIII/Exercicios Fixação - Instruções de Repetição/JogoDeDado.cs
+++ RepositorioGiorgiCoelho/UnidadeVIII/Exercicios Fixação - Instruções de Repetição/JogoDeDado.cs
@@ -1,18 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace UnidadeVIII.Exercicios_Fixação___Instruções_de_Repetição
 {
-    class JogoDeDado
+    internal class JogoDeDado
     {
-        public static void Main(String[] args)
+        public static void Maini(String[] args)
         {
             Random gerador = new Random();
             int dado;
             int soma = 0;
+
             for (int i = 1; i <= 5; i++)
             {
                 dado = gerador.Next(1, 7);
@@ -23,11 +20,14 @@ namespace UnidadeVIII.Exercicios_Fixação___Instruções_de_Repetição
                     break;
                 }
             }
-            Console.WriteLine("Soma: " +soma);
+
+            Console.WriteLine("Soma: " + soma);
+
             if (soma <= 19)
             {
                 Console.WriteLine("Perdeu!");
             }
+
             Console.ReadKey();
         }
     }
---
+++ RepositorioGiorgiCoelho/UnidadeVIII/Exercicios Fixação - Instruções de Repetição/JogoDeDado2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace UnidadeVIII.Exercicios_Fixação___Instruções_de_Repetição
+{
+    internal class JogoDeDado2
+    {
+        public static void Main(string[] args)
+        {
+            int soma = 0;
+            int dado;
+            Random gerador = new Random();
+            for (int i = 1; i <= 4; i++)
+            {
+                dado = gerador.Next(1, 7);
+                if (dado % 2 == 0)
+                {
+                    continue;
+                }
+                soma += dado;
+                if (soma > 9)
+                {
+                    Console.WriteLine("Venceu!");
+                    Console.WriteLine("Pontos: " + soma);
+                    break;
+                }
+            }
+            if (soma <= 9)
+            {
+                Console.WriteLine("Perdeu!");
+                Console.WriteLine("Pontos: " + soma);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
