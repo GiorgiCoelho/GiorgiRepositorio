5af66b6dae784a2eca2094f0caf887b7b65edeb5
GiorgiCoelho <giorgi_coelho@hotmail.com>
2014-05-22 10:08:39 -0300

    Adição Unidade X

---
+++ RepositorioGiorgiCoelho/Unidade_X.cs/ConsumoDeCombustivel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unidade_X.cs
+{
+    class ConsumoDeCombustivel
+    {
+        static void Maini(string[] args)
+        {
+            double km_andado;
+            double litros_carro;
+            double Km_L;
+
+            Console.WriteLine("Quantos litros foram colocados no carro?");
+            litros_carro = double.Parse(Console.ReadLine());
+            Console.WriteLine("Quantos km foram andados?");
+            km_andado = double.Parse(Console.ReadLine());
+            Km_L = CalculoKm(litros_carro, km_andado);
+            Console.WriteLine("Km/L: {0:F2}",Km_L);
+            Console.ReadKey();
+        }
+
+        private static double CalculoKm(double x, double y)
+        {
+            double calculo = y / x;
+            return calculo;
+        }
+    }
+}
---
+++ RepositorioGiorgiCoelho/Unidade_X.cs/JurosCompostos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unidade_X.cs
+{
+    class JurosCompostos
+    {
+        public static void Maini(string[] args)
+        {
+            double montante;
+            double capital;
+            double taxa_juros;
+            double tempo_aplicacao;
+
+            Console.WriteLine("Capital: ");
+            capital = double.Parse(Console.ReadLine());
+            Console.WriteLine("Tempo: ");
+            tempo_aplicacao = double.Parse(Console.ReadLine());
+            Console.WriteLine("Taxa de Juros: ");
+            taxa_juros = double.Parse(Console.ReadLine());
+            taxa_juros = taxa_juros / 100;
+
+            montante = CalculaMontante(capital, tempo_aplicacao, taxa_juros);
+            Console.WriteLine("Montante: {0:F2}",montante);
+            Console.ReadKey();
+        }
+
+        private static double CalculaMontante(double a, double b, double c)
+        {
+            double calculo = a * Math.Pow((1 + c),b);
+            return calculo;
+        }
+
+    }
+}
