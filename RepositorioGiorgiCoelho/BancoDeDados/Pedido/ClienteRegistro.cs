28c95bf951055898536c829f14273c5dea3ec6eb
GiorgiCoelho <giorgi_coelho@hotmail.com>
2014-06-12 09:09:42 -0300

    Cliente - Banco de Dados

--- RepositorioGiorgiCoelho/BancoDeDados/Pedido/ClienteRegistro.cs
+++ RepositorioGiorgiCoelho/BancoDeDados/Pedido/ClienteRegistro.cs
@@ -14,12 +14,84 @@ namespace BancoDeDados.Pedido
         {
             //CONNECTION STRING  
             string connectionString = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\aluno\Source\Repos\GiorgiRepositorio\RepositorioGiorgiCoelho\BancoDeDados\GiorgiBD.mdf;Integrated Security=True";
-
             SqlConnection sqlConnection = new SqlConnection(connectionString);
             //ABRINDO CONEXÃO
             sqlConnection.Open();
 
             //INSERIR PRODUTO
+            InserirDados(sqlConnection);
+
+            //MOSTRA TODOS OS DADOS
+            MostraDados(sqlConnection);
+
+            //ATUALIZA PRODUTO
+            AtualizarDados(sqlConnection);
+
+            //DELETA PRODUTO
+            DeletaDados(sqlConnection);
+
+
+            //FECHA CONEXÃO
+            sqlConnection.Close();
+        }
+
+        private static void DeletaDados(SqlConnection sqlConnection)
+        {
+            Console.WriteLine("Digite o Id do cliente a ser deletado: ");
+            int id = int.Parse(Console.ReadLine());
+            string sqlDelete = String.Format("DELETE from Cliente WHERE Id = {0}", id);
+            SqlCommand command = new SqlCommand(sqlDelete, sqlConnection);
+            try
+            {
+                int i = command.ExecuteNonQuery();
+                if (i > 0)
+                {
+                    Console.WriteLine("Cliente Deletado Com Sucesso!");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Digite algo válido!");
+            }
+        }
+
+        private static void AtualizarDados(SqlConnection sqlConnection)
+        {
+            Console.WriteLine("Digite o ID do Cliente: ");
+            int id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o nome do cliente: ");
+            string nome = Console.ReadLine();
+            string sqlUpdate = String.Format("UPDATE Cliente SET PrimeiroNome = '{0}' WHERE Id = {1}", nome, id);
+            SqlCommand command = new SqlCommand(sqlUpdate, sqlConnection);
+
+            try
+            {
+                int i = command.ExecuteNonQuery();
+                if (i > 0)
+                {
+                    Console.WriteLine("Cliente Atualizado!");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Digite algo válido!");
+            }
+        }
+
+        private static void MostraDados(SqlConnection sqlConnection)
+        {
+            SqlCommand command = new SqlCommand("SELECT * FROM Cliente", sqlConnection);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine(reader["PrimeiroNome"]);
+            }
+        }
+
+        private static void InserirDados(SqlConnection sqlConnection)
+        {
             Console.WriteLine("Digite o Nome: ");
             string nome = (Console.ReadLine());
             Console.WriteLine("Digite o Sobrenome: ");
@@ -48,9 +120,6 @@ namespace BancoDeDados.Pedido
             {
                 Console.WriteLine("Digite algo válido!");
             }
-
-            //FECHA CONEXÃO
-            sqlConnection.Close();
         }
     }
 }
